39758bddc2aa252ee235a6c35fd19824de5d615c
K.H.Park <svclaw2000@gmail.com>
2022-08-25 15:52:14 +0900

    room 관련 파일 추가

---
+++ local/src/main/java/kr/co/alm/mobile_tutorial/local/dao/GithubHistoryDao.kt
@@ -0,0 +1,6 @@
+package kr.co.alm.mobile_tutorial.local.dao
+
+import androidx.room.Dao
+
+@Dao
+interface GithubHistoryDao
---
+++ local/src/main/java/kr/co/alm/mobile_tutorial/local/database/TutorialDatabase.kt
@@ -0,0 +1,22 @@
+package kr.co.alm.mobile_tutorial.local.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import kr.co.alm.mobile_tutorial.local.dao.GithubHistoryDao
+
+@Database(
+    entities = [],
+    version = 1,
+)
+abstract class TutorialDatabase : RoomDatabase() {
+
+    abstract fun githubHistoryDao(): GithubHistoryDao
+
+    fun create(context: Context): TutorialDatabase {
+        return Room.databaseBuilder(context, TutorialDatabase::class.java, "tutorial.db")
+            .fallbackToDestructiveMigrationOnDowngrade()
+            .build()
+    }
+}
