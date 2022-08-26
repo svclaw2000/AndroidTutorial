5d909a3fcb1bd3cda16a3d344d152c3a7cc844a7
K.H.Park <svclaw2000@gmail.com>
2022-08-26 09:06:32 +0900

    Chapter 1 - Step 3-1

--- presentation/src/main/java/kr/co/alm/mobile_tutorial/presentation/ui/home/HomeActivity.kt
+++ presentation/src/main/java/kr/co/alm/mobile_tutorial/presentation/ui/home/HomeActivity.kt
@@ -2,12 +2,30 @@ package kr.co.alm.mobile_tutorial.presentation.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import kr.co.alm.mobile_tutorial.presentation.R
+import kr.co.alm.mobile_tutorial.presentation.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityHomeBinding
+    private val viewModel: HomeViewModel by viewModels()
+
+    private val repositoryListAdapter = RepositoryListAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = repositoryListAdapter
+
+        setViewListener()
+    }
+
+    private fun setViewListener() {
+        TODO("검색을 클릭하면 검색 결과를 화면에 표시한다.")
     }
 }
--- presentation/src/main/java/kr/co/alm/mobile_tutorial/presentation/ui/home/HomeViewModel.kt
+++ presentation/src/main/java/kr/co/alm/mobile_tutorial/presentation/ui/home/HomeViewModel.kt
@@ -1,7 +1,14 @@
 package kr.co.alm.mobile_tutorial.presentation.ui.home
 
 import androidx.lifecycle.ViewModel
+import kr.co.alm.mobile_tutorial.domain.model.Repository
+import kr.co.alm.mobile_tutorial.domain.repository.GithubRepository
 
-class HomeViewModel : ViewModel() {
+class HomeViewModel(
+    private val githubRepository: GithubRepository,
+) : ViewModel() {
 
+    fun fetchRepositoryList(username: String): List<Repository> {
+        TODO("Not yet implemented")
+    }
 }
