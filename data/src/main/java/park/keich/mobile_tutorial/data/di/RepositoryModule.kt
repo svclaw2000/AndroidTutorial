67ab3b2c85580404f39f561fa13d843410b2791d
K.H.Park <svclaw2000@gmail.com>
2022-08-26 17:44:16 +0900

    Chapter02 - Step02

--- data/src/main/java/park/keich/mobile_tutorial/data/di/RepositoryModule.kt
+++ data/src/main/java/park/keich/mobile_tutorial/data/di/RepositoryModule.kt
@@ -5,7 +5,9 @@ import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import park.keich.mobile_tutorial.data.repository.GithubRepositoryImpl
+import park.keich.mobile_tutorial.data.repository.KeywordRepositoryImpl
 import park.keich.mobile_tutorial.domain.repository.GithubRepository
+import park.keich.mobile_tutorial.domain.repository.KeywordRepository
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -13,4 +15,7 @@ abstract class RepositoryModule {
 
     @Binds
     abstract fun bindGithubRepository(impl: GithubRepositoryImpl): GithubRepository
+
+    @Binds
+    abstract fun bindKeywordRepository(impl: KeywordRepositoryImpl): KeywordRepository
 }
--- data/src/main/java/park/keich/mobile_tutorial/data/repository/KeywordRepositoryImpl.kt
+++ data/src/main/java/park/keich/mobile_tutorial/data/repository/KeywordRepositoryImpl.kt
@@ -2,16 +2,17 @@ package park.keich.mobile_tutorial.data.repository
 
 import park.keich.mobile_tutorial.data.source.local.KeywordLocalDataSource
 import park.keich.mobile_tutorial.domain.repository.KeywordRepository
+import javax.inject.Inject
 
-class KeywordRepositoryImpl(
+class KeywordRepositoryImpl @Inject constructor(
     private val local: KeywordLocalDataSource,
 ) : KeywordRepository {
 
     override suspend fun updateKeyword(keyword: String): Result<Unit> {
-        TODO("Not yet implemented")
+        return local.updateKeyword(keyword)
     }
 
     override suspend fun fetchKeyword(): Result<String> {
-        TODO("Not yet implemented")
+        return local.fetchKeyword()
     }
 }
